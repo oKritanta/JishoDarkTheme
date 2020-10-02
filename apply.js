2a916a5d0576fe1c4fe5f87d660ff1f901e82e6f
ヴィクトル <okritanta@gmail.com>
2020-10-01 21:29:28 -0300

    Update origin

--- apply.js
+++ apply.js
@@ -2,5 +2,5 @@ var head = document.getElementsByTagName('HEAD')[0];
 var link = document.createElement('link');
 link.rel = 'stylesheet';
 link.type = 'text/css';
-link.href = 'style.css';
+link.href = 'https://raw.githubusercontent.com/oKritanta/JishoDarkTheme/main/css/apply.css';
 head.appendChild(link);
